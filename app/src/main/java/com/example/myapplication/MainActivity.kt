e7b8046517fcfe3d11014fe3589c10a5822fcda5
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2023-11-26 23:47:10 +0100

    Uppgift1 android

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,35 +3,75 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("banan")
                 }
             }
+
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
+    var counter by remember { mutableStateOf(0) }
+    Column {
+        Text(
+            text = "Start",
             modifier = modifier
-    )
+        )
+        Text(
+            text = "$counter",
+            modifier = modifier
+        )
+
+        Row {
+            Button(onClick = {
+                counter++
+            }) {
+                Text(text = "+")
+            }
+
+            Button(onClick = {
+                counter--
+            }) {
+                Text(text = "-")
+            }
+        }
+
+        Button(onClick = {
+            counter = 0
+        }) {
+            Text(text = "Reset")
+        }
+    }
 }
 
 @Preview(showBackground = true)
